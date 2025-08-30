00b8c529ee8730ba35ea11854c0a21306a7bf94f
ivencomur <ivancortes@hotmail.com>
2025-08-30 02:45:29 -0400

    emptying before commiting

--- src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { WelcomePageComponent } from './welcome-page/welcome-page.component';
-
-export const routes: Routes = [
-  { path: 'welcome', component: WelcomePageComponent },
-  { path: '', redirectTo: 'welcome', pathMatch: 'full' }
-];
